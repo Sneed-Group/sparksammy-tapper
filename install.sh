ec804a7d7a2f4df94e92c3d719ce60c4969fd94f
Maxwell Drake <gorwell1984@noreply.localhost>
2024-04-06 20:34:43 +0000

    Upload files to ''

---
+++ install.sh
@@ -0,0 +1,4 @@
+brew install mas node python3 potrace imagemagick wget minetest
+brew install --cask toontown-rewritten shotcut darktable orion rectangle wine-stable ollama utm hiddenbar pencil2d vscodium discord keepingyouawake freetube dotnet-sdk shottr raycast vlc protonvpn command-x
+mas install 1484403395 1469254409 1474771319 497799835
+sudo spctl --master-disable
