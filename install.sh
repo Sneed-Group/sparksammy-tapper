1c676d2b7de1b4b4baab63e08ac1ab762d65f4b0
Maxwell Drake <gorwell1984@noreply.localhost>
2024-04-08 17:08:58 +0000

    Update 'install.sh'

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
-brew install mas node python3 potrace imagemagick wget minetest golang
+brew install mas node python3 potrace imagemagick wget minetest golang openjdk@17
 brew install --cask toontown-rewritten shotcut darktable orion rectangle wine-stable ollama utm hiddenbar pencil2d vscodium discord keepingyouawake freetube dotnet-sdk shottr raycast vlc protonvpn command-x upscayl imageoptim android-studio android-platform-tools
 sudo npm install -g cordova
 mas install 1484403395 1469254409 1474771319 497799835
