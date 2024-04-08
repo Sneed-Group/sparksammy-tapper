df6e13cdd5b2e311b309576ecfa8c4f32c69125c
Maxwell Drake <gorwell1984@noreply.localhost>
2024-04-08 16:55:13 +0000

    Update 'install.sh'

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 brew install mas node python3 potrace imagemagick wget minetest golang
-brew install --cask toontown-rewritten shotcut darktable orion rectangle wine-stable ollama utm hiddenbar pencil2d vscodium discord keepingyouawake freetube dotnet-sdk shottr raycast vlc protonvpn command-x upscayl imageoptim
+brew install --cask toontown-rewritten shotcut darktable orion rectangle wine-stable ollama utm hiddenbar pencil2d vscodium discord keepingyouawake freetube dotnet-sdk shottr raycast vlc protonvpn command-x upscayl imageoptim android-studio android-platform-tools
 sudo npm install -g cordova
 mas install 1484403395 1469254409 1474771319 497799835
 sudo spctl --master-disable
