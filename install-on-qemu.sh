eb503a524c207abf823a4612f93687414ed6e987
Maxwell Drake <gorwell1984@noreply.localhost>
2024-04-15 10:50:06 +0000

    Add 'install-on-qemu.sh'

---
+++ install-on-qemu.sh
@@ -0,0 +1,8 @@
+brew install mas node python3 potrace imagemagick wget golang openjdk@17 gradle
+brew install --cask rectangle hiddenbar keepingyouawake dotnet-sdk vlc command-x imageoptim macs-fan-control librewolf
+sudo npm install -g cordova
+sudo spctl --master-disable
+defaults write com.apple.TextEdit NSShowAppCentricOpenPanelInsteadOfUntitledFile -bool false
+defaults write -g ApplePressAndHoldEnabled -bool false
+defaults write NSGlobalDomain AppleShowAllExtensions -bool true
+sudo xcodebuild -license accept
