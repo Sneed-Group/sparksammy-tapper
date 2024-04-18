f0fc0ed1218082f74575bc21ac987c018dad7d39
Sam Sneed <163201376+sam-sneed@users.noreply.github.com>
2024-04-18 17:46:38 -0500

    Create install-mini.sh

---
+++ install-mini.sh
@@ -0,0 +1,11 @@
+brew install mas node python3 potrace imagemagick wget golang gradle
+brew tap localsend/localsend
+brew install localsend
+brew install --cask shotcut darktable orion rectangle ollama hiddenbar pencil2d vscodium discord keepingyouawake freetube dotnet-sdk mpv protonvpn command-x upscayl imageoptim android-studio android-platform-tools libreoffice krita
+sudo npm install -g cordova
+mas install 497799835
+sudo spctl --master-disable
+defaults write com.apple.TextEdit NSShowAppCentricOpenPanelInsteadOfUntitledFile -bool false
+defaults write -g ApplePressAndHoldEnabled -bool false
+defaults write NSGlobalDomain AppleShowAllExtensions -bool true
+sudo xcodebuild -license accept
