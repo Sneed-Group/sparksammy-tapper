005c2c084ff23e7d1fe1ad76272158fe0febe81f
The Ghost of FOSS' Future <163201376+sneedgroup-holder@users.noreply.github.com>
2024-08-25 17:36:34 -0500

    use vscode not codium

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 brew install mas node python3 potrace imagemagick wget minetest golang openjdk@17 gradle
 brew tap localsend/localsend
 brew install localsend
-brew install --cask toontown-rewritten shotcut darktable brave-browser rectangle wine-stable ollama utm hiddenbar pencil2d vscodium discord keepingyouawake freetube dotnet-sdk shottr raycast vlc protonvpn command-x upscayl imageoptim android-studio android-platform-tools macs-fan-control libreoffice krita element openemu
+brew install --cask toontown-rewritten shotcut darktable brave-browser rectangle wine-stable ollama utm hiddenbar pencil2d visual-studio-code discord keepingyouawake freetube dotnet-sdk shottr raycast vlc protonvpn command-x upscayl imageoptim android-studio android-platform-tools macs-fan-control libreoffice krita element openemu
 brew install --cask github@beta
 sudo npm install -g cordova
 mas install 1484403395 1469254409 1474771319 497799835
