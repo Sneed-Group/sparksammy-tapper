bf74fd75af1f659f33744d32a7774a96f9ee8688
The Ghost of FOSS' Future <163201376+sneedgroup-holder@users.noreply.github.com>
2024-09-27 13:32:10 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@ brew tap localsend/localsend
 brew install localsend
 brew install --cask toontown-rewritten kdenlive darktable arc wine-stable ollama utm hiddenbar pencil2d vscodium discord dotnet-sdk raycast vlc protonvpn command-x upscayl android-studio android-platform-tools libreoffice krita openemu
 brew install --cask github@beta inkscape virtual-ii
-brew install --cask nvidia-geforce-now roblox robloxstudio blender@lts
+brew install --cask nvidia-geforce-now roblox robloxstudio blender@lts git-credential-manager
 sudo npm install -g cordova
 mas install 937984704 1469254409 1474771319 497799835 682658836
 defaults write com.apple.TextEdit NSShowAppCentricOpenPanelInsteadOfUntitledFile -bool false
