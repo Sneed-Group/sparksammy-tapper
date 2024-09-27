1d7296768e84cdaaf1a05be6752c5d02940721e9
The Ghost of FOSS' Future <163201376+sneedgroup-holder@users.noreply.github.com>
2024-09-27 13:32:36 -0500

    Update install-on-qemu.sh

--- install-on-qemu.sh
+++ install-on-qemu.sh
@@ -1,7 +1,6 @@
 brew install mas node python3 potrace imagemagick wget golang openjdk@17 gradle
 brew install --cask rectangle hiddenbar keepingyouawake dotnet-sdk vlc command-x imageoptim librewolf
 sudo npm install -g cordova
-sudo spctl --master-disable
 defaults write com.apple.TextEdit NSShowAppCentricOpenPanelInsteadOfUntitledFile -bool false
 defaults write -g ApplePressAndHoldEnabled -bool false
 defaults write NSGlobalDomain AppleShowAllExtensions -bool true
